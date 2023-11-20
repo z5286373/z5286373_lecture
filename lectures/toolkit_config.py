644966a56ef4461b55d195c9b0d19765bbc75105
z5286373 <149746159+z5286373@users.noreply.github.com>
2023-11-20 18:03:32 +1100

    Add files via upload

---
+++ lectures/toolkit_config.py
@@ -0,0 +1,8 @@
+"""toolkit_config.py
+Project configuration file
+"""
+import os
+PRJDIR = './'
+ROOTDIR = os.path.join(PRJDIR, 'project1')
+DATDIR = os.path.join(PRJDIR, 'data')
+TICPATH = os.path.join(PRJDIR, 'TICKERS.txt')
